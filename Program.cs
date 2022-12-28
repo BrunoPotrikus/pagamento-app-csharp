b07c0e3c2f97090cb948e22f3d78bfa74c1fde98
BrunoPotrikus <brunopotrikus@gmail.com>
2022-12-27 21:20:59 -0300

    Primeiro commit, com o básico dos pilares da programação orientada a objetos

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Runtime;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var pagamentoBoleto = new PagamentoBoleto();
+        pagamentoBoleto.Pagar();
+        pagamentoBoleto.Vencimento = DateTime.Now;
+
+        Console.Clear();
+        Console.WriteLine(pagamentoBoleto.Vencimento);
+    }
+
+    class Pagamento
+    {
+        public DateTime Vencimento;
+
+        public void Pagar()
+        {
+           
+        }
+    }
+
+    class PagamentoBoleto : Pagamento
+    {
+
+    }
+}
