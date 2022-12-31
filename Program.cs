446dcd2f173c112f5a21926f62e7d9e889f8fabc
BrunoPotrikus <brunopotrikus@gmail.com>
2022-12-30 23:03:03 -0300

    Adicionado listas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Runtime;
+using System.Security.Cryptography.X509Certificates;
 using devPoo.Class.TipoPagamento;
 
 public class Program
@@ -15,8 +16,27 @@ public class Program
             pagamentoBoleto.Pagar(DateTime.Now);
             pagamentoBoleto.CodigoDeBarras = "123456";
 
+            var listaPagamentosBoleto = new List<Boleto>();
+            listaPagamentosBoleto.Add(pagamentoBoleto);
+
+            for (var index  = 0; index < listaPagamentosBoleto.Count; index++)
+            {
+                Console.WriteLine($"Boleto {index}: {listaPagamentosBoleto[index].Vencimento}");
+            }
+
+            // Buscando elemento(s) específico(s) dentro da lista
+            var boletosPagos = listaPagamentosBoleto.Where(x => x.Vencimento <= DateTime.Now);
+
+            // Verificando se elemneto existe na lista
+            var exists = listaPagamentosBoleto.Any(x => x.CodigoDeBarras == "123457");
+
+            // Removendo elemento da lista
+            listaPagamentosBoleto.Remove(pagamentoBoleto);
+
             Console.WriteLine(pagamentoBoleto.VerVencimento(DateTime.Now));
             Console.WriteLine(pagamentoBoleto.CodigoDeBarras);
+            Console.WriteLine(exists);
+            Console.WriteLine(listaPagamentosBoleto.Count);
         }
     }
 }
